f4d15720738b4eb1c071187cc37464da04aa570d
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-06-24 11:14:20 +0530

    test updates

--- test/Selection.js
+++ test/Selection.js
@@ -9,6 +9,18 @@ var chai = require('chai'),  should = chai.should(), expect = chai.expect ,
 var dataItems = [{id:1, name:'name'}, {id:2, name:'two'}, {id:3, name:'three'}];
 
 describe('#singleselect', function() {
+
+    it('selecting returns object', function(){
+        var selection = new Selection();
+        selection.select(dataItems[0]);
+        expect(selection.getSelected()).to.be.a('object')
+    })
+
+    it('selecting returns null when none selected', function(){
+        var selection = new Selection();
+        expect(selection.getSelected()).to.equal(null)
+    })
+
     it('selecting multiple times store only latest selection', function(){
         var selection = new Selection();
         selection.select(dataItems[0]);
@@ -19,6 +31,20 @@ describe('#singleselect', function() {
 
 
 describe('#multiselect', function() {
+
+
+    it('selecting returns object', function(){
+        var selection = new Selection({multiSelect:true});
+        selection.select(dataItems[0]);
+        expect(selection.getSelected()).to.be.a('array')
+    })
+
+
+    it('selecting returns empty array when none selected', function(){
+        var selection = new Selection({multiSelect:true});
+        expect(selection.getSelected()).to.have.length(0);
+    })
+
     it('selecting multiple times store all selection', function(){
         var selection = new Selection({multiSelect:true});
         selection.select(dataItems[0]);
